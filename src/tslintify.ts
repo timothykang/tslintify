873bdb35b5d8fa12e01aa0ecdd89ff6f4487bd31
Tim Kang <timothykang@users.noreply.github.com>
2016-11-07 22:14:24 -0800

    Use TypeScript.getSupportedExtensions()

--- src/tslintify.ts
+++ src/tslintify.ts
@@ -3,30 +3,25 @@ import { readFileSync } from 'fs';
 import { PassThrough, Transform } from 'stream';
 import * as Linter from 'tslint';
 import { findConfiguration } from 'tslint/lib/configuration';
-import * as TypeScript from 'typescript';
+import { findConfigFile, getSupportedExtensions, parseConfigFileTextToJson, sys } from 'typescript';
+
+declare module 'typescript' {
+    // stub method missing from typescript.d.ts
+    // tslint:disable-next-line:no-unused-variable
+    function getSupportedExtensions(options): string[];
+}
 
 export = function (b: BrowserifyObject, options) {
-    const extensions = ['ts'];
-    const tsConfigFile = TypeScript.findConfigFile(options.basedir, TypeScript.sys.fileExists);
-    const tsConfig = TypeScript.parseConfigFileTextToJson(tsConfigFile, readFileSync(tsConfigFile, 'UTF-8')).config;
-
-    if (tsConfig.compilerOptions.allowJs) {
-        extensions.push('js');
-    }
-
-    if (tsConfig.compilerOptions.jsx) {
-        extensions.push('tsx');
-
-        if (tsConfig.compilerOptions.allowJs) {
-            extensions.push('jsx');
-        }
-    }
+    const baseDir = options.basedir || process.cwd();
+    const tsConfigFile = findConfigFile(baseDir, sys.fileExists);
+    const parsed = parseConfigFileTextToJson(tsConfigFile, readFileSync(tsConfigFile, 'UTF-8'));
+    const extensions = getSupportedExtensions(parsed.config.compilerOptions) || [];
 
     b.transform(function (file: InputFile) {
         if (
             typeof file !== 'string' ||
             file.lastIndexOf('.') === -1 ||
-            extensions.indexOf(file.slice(file.lastIndexOf('.') + 1)) === -1
+            extensions.indexOf(file.slice(file.lastIndexOf('.'))) === -1
         ) {
             return new PassThrough();
         }
